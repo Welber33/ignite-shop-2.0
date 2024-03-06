64996ebb19099d2107211ff66d4996fbe9bf122e
Welber33 <jwwork21@gmail.com>
2024-03-06 14:19:44 -0300

    Ignite Shop - Estrutura da Página de Produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,30 @@
 import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/Product";
 
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem unde similique asperiores facere illum exercitationem
+          sunt excepturi, optio earum voluptate quaerat qui deserunt
+          tempora aliquid eos. Veniam facilis doloribus at.
+        </p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
